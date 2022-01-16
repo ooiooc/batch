3f793d687ecbcf2d984e9c100c01ed93763f12f2
ooiooc <shinjh1102@naver.com>
2022-01-16 10:56:48 +0900

    Initial commit

---
+++ src/test/java/com/hyepp/tutorial/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyepp.tutorial.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
